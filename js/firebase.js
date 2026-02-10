2d8da102deed3e0e03c026e0ec74c27bdb42cd39
aquahills <aquahillsbeverages@gmail.com>
2026-02-10 18:49:49 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,5 @@
-/* =========================
-   1. GOOGLE PROJECT DETAILS
-   ========================= */
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBSs9dJyBGo8Rg8i0B2TktSqFvcyGKZbr4",
+  apiKey: "AIzaSyBBs9dJyBGo8Rg8i0B2TktSqfvcyGKZbr4",
   authDomain: "aquahills-3fdcc.firebaseapp.com",
   projectId: "aquahills-3fdcc",
   storageBucket: "aquahills-3fdcc.firebasestorage.app",
@@ -11,17 +7,10 @@ const firebaseConfig = {
   appId: "1:1014475952575:web:5a91df107d3a376548510a"
 };
 
-/* =========================
-   2. START GOOGLE LOGIN
-   ========================= */
-
-// Start Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Get login service
 const auth = firebase.auth();
 
-// Runs when user clicks "Continue with Google"
 function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -29,10 +18,8 @@ function googleLogin() {
     .then(async (result) => {
       const user = result.user;
 
-      // Remember logged-in user
       localStorage.setItem("user", user.email);
 
-      // Save user to Google Sheet
       await api("saveUser", {
         email: user.email,
         name: user.displayName || "",
@@ -40,8 +27,7 @@ function googleLogin() {
         role: "customer"
       });
 
-      // Go back to home page
-      window.location.href = "index.html";
+      window.location.href = "../index.html";
     })
     .catch((error) => {
       alert("Login failed. Please try again.");
