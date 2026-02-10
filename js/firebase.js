189d988ec7b5463e7b10a2cfdd2c9eb8f4cea6a1
aquahills <aquahillsbeverages@gmail.com>
2026-02-10 18:54:00 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,8 +2,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyBBs9dJyBGo8Rg8i0B2TktSqfvcyGKZbr4",
   authDomain: "aquahills-3fdcc.firebaseapp.com",
   projectId: "aquahills-3fdcc",
-  storageBucket: "aquahills-3fdcc.firebasestorage.app",
-  messagingSenderId: "1014475952575",
   appId: "1:1014475952575:web:5a91df107d3a376548510a"
 };
 
